2bbf866cfae2b15aa72105f3340adbc9c9cb0c5e
Shawn <shawn.shaochen.li@gmail.com>
2023-06-09 10:47:09 +1000

    Fix code

--- JS/flood_it_group_project/flood_adjacent.js
+++ JS/flood_it_group_project/flood_adjacent.js
@@ -1,15 +1,15 @@
 function floodAdjacent(col, row) {
     let flooded = false;
-    if ((col+1 <= userSettings.size) && (grid[col+1][row])){
+    if ((col+1 <= userSettings.size) && (grid[col+1][row].Flood)){
         flooded = true;
     }
-    if ((col-1 >= 0) && (grid[col-1][row])){
+    if ((col-1 >= 0) && (grid[col-1][row].Flood)){
         flooded = true;
     }
-    if ((row+1 <= 0) && (grid[col][row+1])){
+    if ((row+1 <= 0) && (grid[col][row+1].Flood)){
         flooded = true;
     }
-    if ((row-1 >= 0) && (grid[col][row-1])){
+    if ((row-1 >= 0) && (grid[col][row-1].Flood)){
         flooded = true;
     }
 
